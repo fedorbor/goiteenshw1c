625fd954f045cf2735dc9fdf86648a2a47a7c226
fedorbor <112242236+fedorbor@users.noreply.github.com>
2023-12-08 18:30:26 +0200

    сс

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,14 @@
-export const App = () => {
-   
-   const myName = 'fedor';
+import React from 'react';
+import Greeting from './Greeting';
+import Message from './Message';
 
- 
-   const favoriteWebsite = { name: 'Google', url: 'https://www.google.com/' };
- 
-   const describe='фотка';
-   const number1 = 5;
-   const number2 = 10;
-   const sum = number1 + number2;
-  const urlpic="https://tourweek.ru/file/image?path=uploads/gallery_media/263_1_%2816%29.jpg&s=b7031d924949ed48603013c83562eae8"
-   
-   const colors = ['Червоний', 'Синій', 'Зелений'];
- 
-   return (
-     <div>
-       {/* Виведення тексту імені у елементі h1 */}
-       <h1>{myName}</h1>
- 
-       {/* Виведення елементу p з текстом "Ласкаво просимо до нашого сайту!" */}
-       <p>Ласкаво просимо до нашого сайту!</p>
- 
-       {/* Виведення зображення у елементі img */}
-       <img src={urlpic} alt={describe} />
- 
-       {/* Виведення посилання у елементі a */}
-       <a href={favoriteWebsite.url}>{favoriteWebsite.name}</a>
- 
-       {/* Виведення результату обчислення суми чисел у елементі p */}
-       <p>Сума чисел {number1} і {number2} дорівнює {sum}.</p>
- 
-       {/* Виведення кольорів у списку ul */}
-       <ul>
-         {colors.map((color, index) => (
-           <li key={index}>{color}</li>
-         ))}
-       </ul>
-     </div>
-   );
+export const App = () => {
+  const userName = 'Іван'; // Замініть це ім'ям користувача, яке ви хочете використовувати
+
+  return (
+    <div>
+      <Greeting name={userName} />
+      <Message text="класс" />
+    </div>
+  );
 };
