743d32b128ddfddcbe5097ba42b762e0263e5b4e
fedorbor <112242236+fedorbor@users.noreply.github.com>
2024-01-19 18:57:51 +0200

    jj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,52 @@
+import React, { Component } from 'react';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Notification from './Notification';
 
-import FriendList from './FriendList';
-import friends from './friends.json';
-import React from 'react';
-// import Statistics from './Statistics'; 
-// import data from './data.json'; 
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
 
-export function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-          <Statistics title="Upload Stats" stats={data} />
-      </header> */}
-      <header className="App-header">
-        <FriendList friends={friends} />
-      </header>
-    </div>
-    
-  );
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const { good } = this.state;
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
+  handleFeedback = (type) => {
+    this.setState((prevState) => ({ [type]: prevState[type] + 1 }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div>
+        <Section title="Please leave your feedback">
+          <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={this.handleFeedback} />
+        </Section>
+
+        {total === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} />
+          </Section>
+        )}
+      </div>
+    );
+  }
 }
 
-
-
+export default App;
